c8f6fd08d7d6f37e727e05b9b17617207175d772
oblevion <commanderp92@gmail.com>
2026-02-18 01:28:14 +0530

    Initial commit

--- config/db.js
+++ config/db.js
@@ -1,19 +1,17 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+const connection = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
-db.connect(err => {
+connection.connect((err) => {
   if (err) {
     console.error("❌ DB CONNECTION FAILED:", err);
   } else {
     console.log("✅ MySQL Connected");
   }
 });
-
-module.exports = db;
