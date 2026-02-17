d66b405e5ef3a3f87b283245668696ef68656612
oblevion <commanderp92@gmail.com>
2026-02-17 22:30:50 +0530

    Initial commit

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,11 +1,10 @@
 const db = require("../config/db");
 
 exports.getRegistrations = (req, res) => {
-  db.query(
-    "SELECT * FROM registrations ORDER BY created_at DESC",
-    (err, results) => {
-      if (err) return res.status(500).json(err);
-      res.json(results);
+  db.query("SELECT * FROM registrations", (err, results) => {
+    if (err) {
+      return res.status(500).json(err);
     }
-  );
+    res.json(results);
+  });
 };
