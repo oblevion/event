31b3a96458ab39b554c6873d7518237bdda8a0fc
oblevion <commanderp92@gmail.com>
2026-02-18 00:49:43 +0530

    Initial commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
   res.send("Backend running successfully 🚀");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, "0.0.0.0",() =>
   console.log(`Server running on port ${PORT}`)
 );
